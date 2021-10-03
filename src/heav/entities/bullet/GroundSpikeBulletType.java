e4cabe59c96c0f26f12dd1d0dd5970c465bb6781
root <root@localhost.localdomain>
2021-10-03 11:48:31 +0000

    hell

--- src/heav/entities/bullet/GroundSpikeBulletType.java
+++ src/heav/entities/bullet/GroundSpikeBulletType.java
@@ -1,7 +1,70 @@
 package heav.entities.bullet;
 
+import mindustry.gen.*;
+import mindustry.content.*;
+import mindustry.entities.*;
 import mindustry.entities.bullet.*;
 
 public class GroundSpikeBulletType extends BasicBulletType{
+	public float groundEffectST = 0f;
+	public float groundBulletSpacing = 0f;
+	public float groundBulletST = 0f;
+	public Effect groundEffect = Fx.none;
+	public BulletType groundBullet = Bullets.standardCopper;
+	public int groundBullets = 2;
 
+	public GroundSpikeBulletType(float speed, float damage, String sprite){
+		super(speed, damage, sprite);
+		absorbable = false;
+		reflectable = false;
+		hittable = false;
+		collides = false;
+		collidesGround = false;
+		collidesAir = false;
+		collidesTiles = false;
+		hitEffect = Fx.none;
+		despawnEffect = Fx.none;
+		shootEffect = Fx.none;
+		smokeEffect = Fx.none;
+	}
+
+	public GroundSpikeBulletType(float speed, float damage){
+		this(speed, damage, "bullet");
+	}
+
+  @Override
+	public void drawLight(Bullet b){
+		//nothing
+	};
+
+  @Override
+	public void draw(Bullet b){
+	  //nothing
+	};
+
+  @Override
+	public void update(Bullet b){
+		if(b.timer.get(0, groundEffectST)){
+			if(groundEffect != null && groundEffect != Fx.none){
+		  	groundEffect.at(b.x, b.y, b.rotation());
+	  	}
+			Sounds.place.at(b.x, b.y, 0.42f, 1f);
+		}
+		if(b.timer.get(1, groundBulletST)){
+			groundEffect.at(b.x, b.y, b.rotation());
+			for(int i = 0; i < groundBullets; i++){
+				float angle = b.rotation() + (i - (groundBullets / 2f)) * groundBulletSpacing;
+				groundBullet.create(b, b.x, b.y, angle);
+			};
+		};
+	};
+
+  @Override
+	public float estimateDPS(){
+    float sum = super.estimateDPS();
+    if(groundBullet != null && groundBullet != this){
+      sum += (groundBullet.estimateDPS() * groundBullets / 2f) * (lifetime / groundBulletST);
+    }
+    return sum;
+	};
 }

ef92ea544c3306db7254ac85c22607aaf19a4040
Zeniku <zjuliuscorpuz@gmail.com>
2022-03-19 19:35:46 +0800

    ye

--- src/heav/HeavyMachineryJava.java
+++ src/heav/HeavyMachineryJava.java
@@ -1,10 +1,6 @@
 package heav;
 
-import arc.*;
-import arc.util.*;
-import mindustry.game.EventType.*;
 import mindustry.mod.*;
-import mindustry.ui.dialogs.*;
 import heav.content.*;
 import static mindustry.Vars.*;
 
@@ -12,19 +8,6 @@ public class HeavyMachineryJava extends Mod{
 
   public HeavyMachineryJava(){
 
-  //listen for game load event
-  Events.on(ClientLoadEvent.class, e -> {
-    //show dialog upon startup
-    Time.runTask(10f, () -> {
-    
-      BaseDialog dialog = new BaseDialog("frog");
-        dialog.cont.add("behold").row();
-        //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-        dialog.cont.image(Core.atlas.find("heavymachineryjava-frog")).pad(20f).row();
-        dialog.cont.button("I see", dialog::hide).size(100f, 50f);
-        dialog.show();
-      });
-    });
   }
 
   @Override
